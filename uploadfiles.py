1a5a4987b602e2c7936aa7bbf65ec4a6fdf82faf
chetan1234-git <71207837+chetan1234-git@users.noreply.github.com>
2022-01-16 14:36:58 +0530

    Update uploadfiles.py

--- uploadfiles.py
+++ uploadfiles.py
@@ -16,9 +16,9 @@ def main():
     transferData = TransferData(access_token)
 
     file_from = input("Enter the file path to transfer : -")
-    file_to = input("enter the full path to upload to dropbox:- ")  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
-
-    # API v2
+    file_to = input("enter the full path to upload to dropbox:- ") 
+    
+   
     transferData.upload_file(file_from, file_to)
     print("file has been moved !!!")
 
